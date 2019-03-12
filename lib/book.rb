ec05ff180441c291c04a2b87a621b7561862de24
Theo Weller <weller.theodor@gmail.com>
2019-03-12 21:49:35 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,18 +1,18 @@
 class Book
-  attr_accessor :author, :page_count,
+  attr_accessor :author, :page_count
   attr_reader :title
 
   def initialize(title)
     @title = title
   end
 
-  def genre=(genre)
-    @genre = genre
-    GENRES << genre
-  end
-
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+  
 end
