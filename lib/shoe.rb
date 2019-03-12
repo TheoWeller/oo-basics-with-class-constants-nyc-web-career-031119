4e52aa88037cdb6b33cdaa4a37e4d54dca5f7f43
Theo Weller <weller.theodor@gmail.com>
2019-03-12 22:03:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,15 +7,15 @@ class Shoe
   BRANDS = []
 
   def initialize(brand)
-    @brand = brand
-    BRANDS << brand
+    if !BRANDS.include?(brand)
+      @brand = brand
+      BRANDS << brand
+    end
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-BRANDS.uniq
 
 end
